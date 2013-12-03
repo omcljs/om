cfb463996f94707525ee88ac8995d0908bad4c62
David Nolen <david.nolen@gmail.com>
2013-12-02 23:13:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reactjs
+
+Wrapping up React.js into a JAR
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject reactjs "0.5.1"
+  :description "A Javascript Library for building user interfaces"
+  :url "http://facebook.github.io/react/"
+  :license {:name "Apache"
+            :url "http://www.apache.org/licenses/"}
+  :source-paths  ["src"]
+  :dependencies [])
