ec5f7890d85295e8ffc942c2c873b55e5080dc96
David Nolen <david.nolen@gmail.com>
2013-12-13 01:11:06 -0500

    super sophisticated update batching technology ;)

--- src/om/core.cljs
+++ src/om/core.cljs
@@ -3,16 +3,25 @@
             [om.vars :as vars]
             [om.dom :as dom :include-macros true]))
 
+(def refresh-queued false)
+
 (defn root [value f target]
   (let [state (if (instance? Atom value)
                 value
                 (atom value))
         rootf (fn []
+                (set! refresh-queued false)
                 (binding [vars/*state* state]
                   (dom/render
                     (dom/pure @state (f @state []))
                     target)))]
-    (add-watch state ::root (fn [_ _ _ _] (rootf)))
+    (add-watch state ::root
+      (fn [_ _ _ _]
+        (when-not refresh-queued
+          (set! refresh-queued true)
+          (if (exists? js/requestAnimationFrame)
+            (js/requestAnimationFrame rootf)
+            (js/setTimeout rootf 16)))))
     (rootf)))
 
 (defn render
