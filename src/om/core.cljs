5ea65d7bb38aa57d62362aea9843a73d834116b5
David Nolen <david.nolen@gmail.com>
2013-12-13 14:35:39 -0500

    unroll update!

--- src/om/core.cljs
+++ src/om/core.cljs
@@ -29,7 +29,7 @@
   ([f data key] (render data nil key))
   ([f data opts key]
     (if (keyword-identical? key ::no-key)
-      (dom/pure #js {:value data :opts opts} (f data path))
+      (dom/pure #js {:value data :opts opts} (f data))
       (let [data (get data key)
             new-path (conj path key)]
         (dom/pure #js {:value data :opts opts}
@@ -39,6 +39,22 @@
   (let [data (vary-meta data assoc ::owner vars/*owner*)]
     (fn [e] (f e data))))
 
-(defn update! [data f k val]
-  (let [m (meta data)]
-    (swap! (::state meta) update-in (conj (::path meta) k) f val)))
+(defn update!
+  ([data k f]
+    (let [m (meta data)]
+      (swap! (::state meta) update-in (conj (::path meta) k) f)))
+  ([data k f a]
+    (let [m (meta data)]
+      (swap! (::state meta) update-in (conj (::path meta) k) f a)))
+  ([data k f a b]
+    (let [m (meta data)]
+      (swap! (::state meta) update-in (conj (::path meta) k) f a b)))
+  ([data k f a b c]
+    (let [m (meta data)]
+      (swap! (::state meta) update-in (conj (::path meta) k) f a b c)))
+  ([data k f a b c d]
+    (let [m (meta data)]
+      (swap! (::state meta) update-in (conj (::path meta) k) f a b c d)))
+  ([data k f a b c d & args]
+    (let [m (meta data)]
+      (apply swap! (::state meta) update-in (conj (::path meta) k) f a b c d args))))
