f1a7c02ddbc263a0df2abddc4cf3dfff61a07c97
David Nolen <david.nolen@gmail.com>
2013-12-27 20:23:51 -0500

    backtrack cljs version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :source-paths  ["src"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2131" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2127" :scope "provided"]
                  [com.facebook/react "0.8.0.1"]]
 
   :plugins [[lein-cljsbuild "1.0.0"]]
