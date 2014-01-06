5677fd4b036480e9254b002f563c3ec13c41903f
David Nolen <david.nolen@gmail.com>
2014-01-06 11:27:29 -0500

    snapshot version so as not to collide w/ clojars version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om "0.1.4"
+(defproject om "0.1.5-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
