7dea635e0e32f3bca77d8804a55f9dd1f2c2c4c0
David Nolen <david.nolen@gmail.com>
2014-02-14 01:18:12 -0500

    next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om "0.4.0"
+(defproject om "0.4.1-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
