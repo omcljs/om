f97e004b2fdd04443bedea5c374a27929143a229
David Nolen <david.nolen@gmail.com>
2014-02-27 12:25:11 -0500

    make instrument example a little bit more interesting

--- examples/instrument/src/core.cljs
+++ examples/instrument/src/core.cljs
@@ -4,11 +4,11 @@
 
 (enable-console-print!)
 
-(defn sub-view [data owner]
+(defn sub-view [item owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil "Some subview!"))))
+      (dom/div nil (:text item)))))
 
 (defn app-view [app owner]
   (reify
@@ -16,7 +16,8 @@
     (render [_]
       (dom/div nil
         (dom/div nil (:text app))
-        (om/build sub-view {:foo 'bar})))))
+        (apply dom/ul nil
+          (om/build-all sub-view (:list app)))))))
 
 (defn something-else [original owner opts]
   (reify
@@ -24,13 +25,19 @@
     (render [_]
       (dom/div #js {:style #js {:border "1px solid #ccc"
                                 :padding "5px"}}
-        (dom/div nil "Instrumented!")
+        (dom/div nil
+          (dom/span nil "Path:")
+          (dom/pre #js {:style #js {:display "inline-block"}}
+            (pr-str (om/path (second original)))))
         (apply om/build* original)))))
 
 (om/root
   app-view
-  (atom {:text "Hello world!"})
+  (atom {:text "Instrument!"
+         :list [{:text "Milk"} {:text "Cookies"} {:text "Apples"}]})
   {:target (.getElementById js/document "app")
    :instrument
    (fn [f cursor m]
-     (om/build* something-else [f cursor m]))})
+     (if (= f sub-view)
+       (om/build* something-else [f cursor m])
+       ::om/pass))})
