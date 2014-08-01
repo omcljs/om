fb3e225c1adeab0e2cc60ab67d036e5740a4b4fb
David Nolen <dnolen@cognitect.com>
2014-08-01 14:42:32 -0400

    0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om "0.6.5-SNAPSHOT"
+(defproject om "0.7.0-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
