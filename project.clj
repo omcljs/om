614b0a02d679899567ebb75b65445aff8625925f
David Nolen <david.nolen@gmail.com>
2015-01-21 21:29:27 -0500

    change group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.om/om "0.8.1"
+(defproject org.omcljs/om "0.8.1"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
