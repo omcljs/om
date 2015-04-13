f0256aa6e6ad074d15050203c0306e16f96b3618
David Nolen <david.nolen@gmail.com>
2015-04-12 20:05:52 -0400

    helper for collecting statics

--- dev/om/dev.clj
+++ dev/om/dev.clj
@@ -1,1 +1,35 @@
 (ns om.dev)
+
+(defn collect-statics [dt]
+  (letfn [(split-on-static [forms]
+            (split-with (complement '#{static}) forms))
+          (split-on-symbol [forms]
+            (split-with (complement symbol?) forms))]
+    (loop [dt (seq dt) dt' [] statics {:fields {} :protocols []}]
+      (if dt
+        (let [[pre [_ sym & remaining :as post]] (split-on-static dt)
+              dt' (into dt' pre)]
+          (if (seq post)
+            (cond
+              (= sym 'field)
+              (let [[field-info dt] (split-at 2 remaining)]
+                (recur (seq dt) dt'
+                  (update-in statics [:fields] conj (vec field-info))))
+              (symbol? sym)
+              (let [[protocol-info dt] (split-on-symbol remaining)]
+                (recur (seq dt) dt'
+                  (update-in statics [:protocols]
+                    into (concat [sym] protocol-info))))
+              :else (throw (IllegalArgumentException. "Malformed static")))
+            (recur nil dt' statics)))
+        {:dt dt' :static statics}))))
+
+(comment
+  (collect-statics
+    '(static IFoo
+      (foo [_])
+      (bar [_])
+      static field sel [:woz ?noz]
+      Object
+      (toString [_])))
+  )
