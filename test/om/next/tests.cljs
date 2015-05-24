9d68fce508d9f74945e763cefcf07d28d8b88b17
David Nolen <david.nolen@gmail.com>
2015-05-23 21:53:42 -0400

    test components

--- test/om/next/tests.cljs
+++ test/om/next/tests.cljs
@@ -1,3 +1,25 @@
 (ns om.next.tests
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
-            [om.next :as om]))
+            [om.next :as om :refer-macros [defui]]))
+
+(defui Component
+  static om/IQuery
+  (queries [this]
+    {:self [:foo/bar :baz/woz]})
+  Object
+  (render [this]))
+
+(defui ComponentList
+  static om/IQueryParams
+  (params [this]
+    {:components {:component (om/complete-query Component)}})
+  static om/IQuery
+  (queries [this]
+    '{:components ?component})
+  Object
+  (render [this]))
+
+(comment
+  (om/get-query Component)
+  (om/get-query ComponentList)
+  )est
