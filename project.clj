51e67a34759777ff04204327c4c591fd1c6f4c42
David Nolen <david.nolen@gmail.com>
2015-06-06 10:05:52 -0400

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.omcljs/om "0.9.1"
+(defproject org.omcljs/om "0.9.0-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
