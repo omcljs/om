9e21e3bb9bc9fefbdb6956197579554f1fad84a8
dnolen <dnolen@cognitect.com>
2015-08-05 19:14:35 -0400

    bump deps

--- project.clj
+++ project.clj
@@ -11,8 +11,8 @@
   :source-paths  ["src" "dev"]
 
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "0.0-3308" :classifier "aot" :scope "provided"
-                  :exclusions [ org.clojure/data.json]]
+                 [org.clojure/clojurescript "1.7.48" :classifier "aot" :scope "provided"
+                  :exclusions [org.clojure/data.json]]
                  [org.clojure/data.json "0.2.6" :classifier "aot" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
                  [cljsjs/react "0.13.3-0"]]
