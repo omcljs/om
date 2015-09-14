b0b4ea2ba3fa3213b89d6b12d170877198e6a736
dnolen <dnolen@cognitect.com>
2015-09-14 19:15:41 -0400

    testing the indexer

--- test/om/next/tests.cljs
+++ test/om/next/tests.cljs
@@ -1,6 +1,7 @@
 (ns om.next.tests
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
             [om.next :as om :refer-macros [defui]]
+            [om.next.protocols :as p]
             [om.dom :as dom]))
 
 (defui Component
@@ -16,8 +17,7 @@
     {:component (om/get-query Component)})
   static om/IQuery
   (query [this]
-    ;; '{:components ?component/self}
-    '[{:components/list ?component}])
+    '[{:components/list ?component} :app/title])
   Object
   (render [this]))
 
@@ -25,14 +25,25 @@
   (is (= (om/query Component)
          '[:foo/bar :baz/woz]))
   (is (= (om/query ComponentList)
-         '[{:components/list ?component}])))
+         '[{:components/list ?component} :app/title])))
 
 (deftest test-get-query
   (is (= (om/get-query Component)
          '[:foo/bar :baz/woz]))
   (is (= (om/get-query ComponentList)
-         '[{:components/list [:foo/bar :baz/woz]}])))
+         '[{:components/list [:foo/bar :baz/woz]} :app/title])))
+
+(deftest test-indexer
+  (let [idxr (om/indexer identity)
+        idxs (p/index-root idxr ComponentList)]
+    (is (= (set (keys (:prop->component idxs)))
+           #{:app/title :components/list :foo/bar :baz/woz}))))
 
 (comment
   (run-tests)
+
+  (require '[cljs.pprint :as pp])
+
+  (let [idxr (om/indexer identity)]
+    (pp/pprint (p/index-root idxr ComponentList)))
   )
