69eaf9d4c86225908c3d2edc6ee2ca110beb7614
David Nolen <david.nolen@gmail.com>
2015-09-24 11:05:10 -0500

    state-path -> state-path*

--- test/om/next/tests.cljs
+++ test/om/next/tests.cljs
@@ -78,7 +78,7 @@
          '[({:todos/list [:title]} {:index 1})])))
 
 (deftest test-state-path
-  (is (= (om/state-path [{:todos/list [:title]}] '(* 1))
+  (is (= (om/state-path* [{:todos/list [:title]}] '(* 1))
         '[:todos/list 1])))
 
 ;; -----------------------------------------------------------------------------
