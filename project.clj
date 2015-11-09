6286ec0f190f7d1df5ca7daadcd96c227359abb1
David Nolen <david.nolen@gmail.com>
2015-11-09 09:02:50 -0500

    next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.omcljs/om "1.0.0-alpha19-SNAPSHOT"
+(defproject org.omcljs/om "1.0.0-alpha20-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
