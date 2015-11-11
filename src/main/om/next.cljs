2af27beac82c642efbd43721959071279284fe96
dnolen <dnolen@cognitect.com>
2015-11-11 17:13:38 -0500

    actually deref :root

--- src/main/om/next.cljs
+++ src/main/om/next.cljs
@@ -1159,7 +1159,7 @@
     (p/queue! reconciler keys)
     (reset! state
       (if-let [migrate (:migrate config)]
-        (migrate next (get-query (-> reconciler :state :root)) tempids)
+        (migrate next (get-query (:root @(:state reconciler))) tempids)
         next))))
 
 (defrecord Reconciler [config state]
