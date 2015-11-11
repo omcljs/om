b17ea7ace4fa76adf76597ef15c2fc316eb2ee4d
dnolen <dnolen@cognitect.com>
2015-11-11 15:45:35 -0500

    add reader & writer to om.next.server ns

--- src/main/om/next/server.clj
+++ src/main/om/next/server.clj
@@ -1,5 +1,21 @@
 (ns om.next.server
-  (:require [om.next.impl.parser :as parser]))
+  (:require [om.next.impl.parser :as parser]
+            [om.transit :as transit]))
 
-(defn parser [opts]
+(defn parser
+  "Create a parser. The argument is a map of two keys, :read and :mutate. Both
+   functions should have the signature (Env -> Key -> Params -> ParseResult)."
+  [opts]
   (parser/parser (assoc opts :elide-paths true)))
+
+(defn reader
+  "Create a Om Next transit reader. This reader can handler the tempid type.
+   Can pass transit reader customization opts map."
+  ([in] (transit/reader in))
+  ([in opts] (transit/reader in opts)))
+
+(defn writer
+  "Create a Om Next transit reader. This writer can handler the tempid type.
+   Can pass transit writer customization opts map."
+  ([out] (transit/writer out))
+  ([out opts] (transit/writer out opts)))
