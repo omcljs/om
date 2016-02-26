4b8eedbe1ae60df8fd8dcb5affdc89d33bd04bd0
Tony Kay <tony.kay@gmail.com>
2016-02-26 14:54:54 -0800

    Added client timestamp as metadata to history entries.

--- src/main/om/next/cache.cljs
+++ src/main/om/next/cache.cljs
@@ -3,10 +3,11 @@
 (deftype Cache [arr index size]
   Object
   (add [this id x]
-    (if (<= size (alength arr))
-      (let [id' (.shift arr)]
-        (swap! index #(-> % (dissoc id') (assoc id x))))
-      (swap! index assoc id x))
+    (let [x' (vary-meta x assoc :client-time (js/Date.))]
+      (if (<= size (alength arr))
+        (let [id' (.shift arr)]
+          (swap! index #(-> % (dissoc id') (assoc id x'))))
+        (swap! index assoc id x')))
     (.push arr id))
   (get [this id]
     (get @index id)))
