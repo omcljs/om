1ce5b0dae35e0a8d19d9a9bb35ed85f7fdc4f0ed
dnolen <dnolen@cognitect.com>
2016-03-06 17:29:08 -0500

    1.0.0-alpha31

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.omcljs/om "1.0.0-alpha31-SNAPSHOT"
+(defproject org.omcljs/om "1.0.0-alpha31"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
