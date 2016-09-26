3a8229c4a843c9978ced8eec94f0fa33ae57b869
dnolen <dnolen@cognitect.com>
2016-09-26 17:56:42 -0400

    next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.omcljs/om "1.0.0-alpha46"
+(defproject org.omcljs/om "1.0.0-alpha47-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
