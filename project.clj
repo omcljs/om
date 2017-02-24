1067ede02a85861aeb59ba6963bbeda9b5821e04
dnolen <dnolen@cognitect.com>
2017-02-24 13:23:00 -0500

    bump provided deps for testing

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
 
   :source-paths  ["src/main" "src/devcards" "src/test"]
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.293" :scope "provided" :classifier "aot"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.473" :scope "provided" :classifier "aot"
                   :exclusions [org.clojure/clojure
                                org.clojure/data.json]]
                  [org.clojure/data.json "0.2.6" :scope "provided" :classifier "aot"]
